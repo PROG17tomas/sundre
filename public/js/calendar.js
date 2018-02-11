d374fc7bf93cbad5e649665637552fd4fdadeaf1
Alexander Panagiotis Arvanitis <31844503+Kamiljen@users.noreply.github.com>
2018-02-11 20:08:59 +0100

    Uppstädning efter merge

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,29 +1,29 @@
-$(function() {
-    var currentYear = new Date().getFullYear();
-    $('#calendar').calendar({ 
-        disabledDays: [
-            new Date(currentYear,1,2),
-            new Date(currentYear,1,3),
-            new Date(currentYear,1,8),
-            new Date(currentYear,1,9),
-            new Date(currentYear,1,10),
-            new Date(currentYear,1,11),
-            new Date(currentYear,1,13),
-            new Date(currentYear,1,14),
-            new Date(currentYear,1,15)
-        ],
-        dataSource: [
-            {
-                id: 0,
-                name: 'bajskorv',
-                location: 'där bajskorvar bor 22',
-                startDate: new Date(currentYear, 4, 28),
-                endDate: new Date(currentYear, 4, 29)
-            }],
+// $(function() {
+//     var currentYear = new Date().getFullYear();
+//     $('#calendar').calendar({ 
+//         disabledDays: [
+//             new Date(currentYear,1,2),
+//             new Date(currentYear,1,3),
+//             new Date(currentYear,1,8),
+//             new Date(currentYear,1,9),
+//             new Date(currentYear,1,10),
+//             new Date(currentYear,1,11),
+//             new Date(currentYear,1,13),
+//             new Date(currentYear,1,14),
+//             new Date(currentYear,1,15)
+//         ],
+//         dataSource: [
+//             {
+//                 id: 0,
+//                 name: 'bajskorv',
+//                 location: 'där bajskorvar bor 22',
+//                 startDate: new Date(currentYear, 4, 28),
+//                 endDate: new Date(currentYear, 4, 29)
+//             }],
     
-        displayWeekNumber: true
-    });
-});
+//         displayWeekNumber: true
+//     });
+// });
 
 // function getBookedWeeksFromDb(){
 //     var bookedWeeks = [];
