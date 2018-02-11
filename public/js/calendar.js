c311c65ca0f648a37ee81aef0ac56d1059340981
Alexander Panagiotis Arvanitis <31844503+Kamiljen@users.noreply.github.com>
2018-02-11 19:56:16 +0100

    mina ändringar jag aldrig pushade tydligen

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,6 +1,5 @@
 $(function() {
     var currentYear = new Date().getFullYear();
-    var data = getData();
     $('#calendar').calendar({ 
         disabledDays: [
             new Date(currentYear,1,2),
@@ -25,3 +24,45 @@ $(function() {
         displayWeekNumber: true
     });
 });
+
+// function getBookedWeeksFromDb(){
+//     var bookedWeeks = [];
+//     var obj = getDbData();
+//     obj.forEach(x => {
+//         var tmp = x.vecka.split(',');
+//         tmp.forEach(y => {
+//             bookedWeeks.push(y);
+//         });
+//         return bookedWeeks;
+//     });}
+
+function getDateRangeOfWeek(weekNo){
+    var d1 = new Date();
+    numOfdaysPastSinceLastMonday = eval(d1.getDay()- 1);
+    d1.setDate(d1.getDate() - numOfdaysPastSinceLastMonday);
+    var weekNoToday = d1.getWeek();
+    var weeksInTheFuture = eval( weekNo - weekNoToday );
+    d1.setDate(d1.getDate() + eval( 7 * weeksInTheFuture ));
+    var rangeIsFrom = eval(d1.getMonth()+1) +"," + d1.getDate();
+    d1.setDate(d1.getDate() + 6);
+    var rangeIsTo = eval(d1.getMonth()+1) +"," + d1.getDate();
+    return rangeIsFrom + "/" + rangeIsTo;
+};
+
+Date.prototype.getWeek = function() {
+var onejan = new Date(getFullYear(),0,1);
+return Math.ceil((((this - onejan) / 86400000) + onejan.getDay()+1)/7);
+}
+
+(function(){
+    // var bookedDates = [];
+    // var poop = getBookedWeeksFromDb();
+    // var poop = ["2,4/2,11", "3,4/3,11"]
+    // poop.forEach(o => {
+    //     bookedDates.push(getDateRangeOfWeek(o));
+    // });
+    // poop.forEach(u => {
+    //     console.log(u);
+    // })
+    console.log("bajs");
+})();
